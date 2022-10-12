c6affe01ae29a7c74eee4b9daea6cd517f7efce7
Morten Johansson <morten1.kodehode@gmail.com>
2022-10-12 10:18:52 +0200

    finished js

---
+++ js/data.js
@@ -0,0 +1,84 @@
+const planets = [
+  {
+    name: "Mercury",
+    image: "images/mercury.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Venus",
+    image: "images/venus.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Earth",
+    image: "images/earth.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Mars",
+    image: "images/mars.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Jupiter",
+    image: "images/jupiter.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Saturn",
+    image: "images/saturn3.png",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Uranus",
+    image: "images/uranus.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+  {
+    name: "Neptune",
+    image: "images/neptune.jpg",
+    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
+    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
+    architecto? Officiis laboriosam asperiores earum debitis non,
+    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
+    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+  },
+];
+
+export default planets;
