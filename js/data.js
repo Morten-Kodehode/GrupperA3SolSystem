8da34d56b7318998dee9a9a6a6f4dae62f409635
Arild-BM <arildb.kodehode@gmail.com>
2022-10-12 12:38:29 +0200

    Updated data.js

--- js/data.js
+++ js/data.js
@@ -3,31 +3,26 @@ const planets = [
     name: "Mercury",
     image: "images/mercury.png",
     info: `Mercury is the closest planet to the Sun. The smallest planet 
-    in the Solar System (0.055 MEarth), Mercury has no natural satellites. 
+    in the Solar System, Mercury has no natural satellites. 
     The dominant geological features are impact craters or basins with ejecta 
     blankets, the remains of early volcanic activity including magma flows, 
     and lobed ridges or rupes that were probably produced by a period of 
     contraction early in the planet's history. Mercury's very tenuous atmosphere 
     consists of solar-wind particles trapped by Mercury's magnetic field, as 
     well as atoms blasted off its surface by the solar wind. Its relatively 
-    large iron core and thin mantle have not yet been adequately explained. 
-    Hypotheses include that its outer layers were stripped off by a giant impact, 
-    or that it was prevented from fully accreting by the young Sun's energy.`,
+    large iron core and thin mantle have not yet been adequately explained.`,
   },
   {
     name: "Venus",
     image: "images/venus.png",
-    info: `Venus is close in size to Earth (0.815 MEarth) and, like Earth, 
+    info: `Venus is close in size to Earth and, like Earth, 
     has a thick silicate mantle around an iron core, a substantial atmosphere, 
     and evidence of internal geological activity. It is much drier than Earth, 
     and its atmosphere is ninety times as dense. Venus has no natural satellites. 
     It is the hottest planet, with surface temperatures over 400 °C, mainly due to 
     the amount of greenhouse gases in the atmosphere. The planet has no magnetic 
     field that would prevent depletion of its substantial atmosphere, which suggests 
-    that its atmosphere is being replenished by volcanic eruptions. A relatively young 
-    planetary surface displays extensive evidence of volcanic activity, but is devoid of 
-    plate tectonics. It may undergo resurfacing episodes on a time scale of 700 million 
-    years.`,
+    that its atmosphere is being replenished by volcanic eruptions.`,
   },
   {
     name: "Earth",
@@ -44,7 +39,7 @@ const planets = [
   {
     name: "Mars",
     image: "images/mars.png",
-    info: `Mars is smaller than Earth and Venus (0.107 MEarth). It has an atmosphere of mostly 
+    info: `Mars is smaller than Earth and Venus. It has an atmosphere of mostly 
     carbon dioxide with a surface pressure of 6.1 millibars; roughly 0.6% of that of Earth but 
     sufficient to support weather phenomena. Its surface, peppered with volcanoes, such as Olympus 
     Mons, and rift valleys, such as Valles Marineris, shows geological activity that may have 
@@ -55,7 +50,7 @@ const planets = [
   {
     name: "Jupiter",
     image: "images/jupiter.png",
-    info: `Jupiter, at 318 MEarth, is 2.5 times the mass of all the other planets put together. 
+    info: `Jupiter, is 2.5 times the mass of all the other planets put together. 
     It is composed largely of hydrogen and helium. Jupiter's strong internal heat creates 
     semi-permanent features in its atmosphere, such as cloud bands and the Great Red Spot. 
     The planet possesses a 4.2-14 Gauss strength magnetosphere that spans 22-29 million km, 
@@ -70,19 +65,19 @@ const planets = [
     image: "images/saturn3.png",
     info: `Saturn, distinguished by its extensive ring system, has several similarities 
     to Jupiter, such as its atmospheric composition and magnetosphere. Although Saturn 
-    has 60% of Jupiter's volume, it is less than a third as massive, at 95 MEarth. Saturn 
+    has 60% of Jupiter's volume, it is less than a third as massive. Saturn 
     is the only planet of the Solar System that is less dense than water. The rings of 
     Saturn are made up of small ice and rock particles. Saturn has 83 confirmed satellites 
     composed largely of ice. Two of these, Titan and Enceladus, show signs of geological 
     activity; they, as well as five other Saturnian moons (Iapetus, Rhea, Dione, Tethys, 
-      and Mimas), are large enough to be round. Titan, the second-largest moon in the Solar 
-      System, is bigger than Mercury and the only satellite in the Solar System to have a 
-      substantial atmosphere.`,
+    and Mimas), are large enough to be round. Titan, the second-largest moon in the Solar 
+    System, is bigger than Mercury and the only satellite in the Solar System to have a 
+    substantial atmosphere.`,
   },
   {
     name: "Uranus",
     image: "images/uranus.png",
-    info: `Uranus, at 14 MEarth, has the lowest mass of the outer planets. Uniquely among 
+    info: `Uranus, has the lowest mass of the outer planets. Uniquely among 
     the planets, it orbits the Sun on its side; its axial tilt is over ninety degrees to 
     the ecliptic. This gives the planet extreme seasonal variation as each pole points toward 
     and then away from the Sun. It has a much colder core than the other giant planets and 
@@ -94,7 +89,7 @@ const planets = [
   {
     name: "Neptune",
     image: "images/neptune.png",
-    info: `Neptune, though slightly smaller than Uranus, is more massive (17 MEarth) and 
+    info: `Neptune, though slightly smaller than Uranus, is more massive and 
     hence more dense. It radiates more internal heat than Uranus, but not as much as Jupiter 
     or Saturn. Neptune has 14 known satellites. The largest, Triton, is geologically active, 
     with geysers of liquid nitrogen. Triton is the only large satellite with a retrograde orbit, 
