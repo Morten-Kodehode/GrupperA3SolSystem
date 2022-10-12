3986d39a6bf8e3ab7715cf3e091532161102b8fd
Arild-BM <arildb.kodehode@gmail.com>
2022-10-12 11:02:54 +0200

    Updated data.js

--- js/data.js
+++ js/data.js
@@ -2,82 +2,106 @@ const planets = [
   {
     name: "Mercury",
     image: "images/mercury.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Mercury is the closest planet to the Sun. The smallest planet in the 
+    Solar System (0.055 MEarth), Mercury has no natural satellites. The dominant 
+    geological features are impact craters or basins with ejecta blankets, the 
+    remains of early volcanic activity including magma flows, and lobed ridges 
+    or rupes that were probably produced by a period of contraction early in the 
+    planet's history. Mercury's very tenuous atmosphere consists of solar-wind 
+    particles trapped by Mercury's magnetic field, as well as atoms blasted off 
+    its surface by the solar wind. Its relatively large iron core and thin mantle 
+    have not yet been adequately explained. Hypotheses include that its outer layers 
+    were stripped off by a giant impact, or that it was prevented from fully accreting 
+    by the young Sun's energy.`,
   },
   {
     name: "Venus",
     image: "images/venus.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Venus is close in size to Earth (0.815 MEarth) and, like Earth, has 
+    a thick silicate mantle around an iron core, a substantial atmosphere, and 
+    evidence of internal geological activity. It is much drier than Earth, and 
+    its atmosphere is ninety times as dense. Venus has no natural satellites. It 
+    is the hottest planet, with surface temperatures over 400 °C, mainly due to 
+    the amount of greenhouse gases in the atmosphere. The planet has no magnetic 
+    field that would prevent depletion of its substantial atmosphere, which suggests 
+    that its atmosphere is being replenished by volcanic eruptions. A relatively young 
+    planetary surface displays extensive evidence of volcanic activity, but is devoid 
+    of plate tectonics. It may undergo resurfacing episodes on a time scale of 700 million years.`,
   },
   {
     name: "Earth",
     image: "images/earth.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Earth is the largest and densest of the inner planets, the only one
+     known to have current geological activity, and the only place where life is 
+     known to exist. Its liquid hydrosphere is unique among the terrestrial planets, 
+     and it is the only planet where plate tectonics has been observed. Earth's 
+     atmosphere is radically different from those of the other planets, having been 
+     altered by the presence of life to contain 21% free oxygen. The planetary 
+     magnetosphere shields the surface from solar and cosmic radiation, limiting 
+     atmospheric stripping and maintaining habitability. It has one natural satellite, 
+     the Moon, the only large satellite of a terrestrial planet in the Solar System.`,
   },
   {
     name: "Mars",
     image: "images/mars.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Mars is smaller than Earth and Venus (0.107 MEarth). It has an atmosphere 
+    of mostly carbon dioxide with a surface pressure of 6.1 millibars; roughly 0.6% 
+    of that of Earth but sufficient to support weather phenomena. Its surface, peppered 
+    with volcanoes, such as Olympus Mons, and rift valleys, such as Valles Marineris, 
+    shows geological activity that may have persisted until as recently as 2 million 
+    years ago. It's red colour comes from iron oxide (rust) in its soil. Mars has two 
+    tiny natural satellites (Deimos and Phobos) thought to be either captured asteroids, 
+    or ejected debris from a massive impact early in Mars's history.`,
   },
   {
     name: "Jupiter",
     image: "images/jupiter.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Jupiter, at 318 MEarth, is 2.5 times the mass of all the other planets put together. 
+    It is composed largely of hydrogen and helium. Jupiter's strong internal heat creates 
+    semi-permanent features in its atmosphere, such as cloud bands and the Great Red Spot. 
+    The planet possesses a 4.2-14 Gauss strength magnetosphere that spans 22-29 million km, 
+    making it, in certain respects, the largest object in the Solar System. Jupiter has 80 
+    known satellites. The four largest, Ganymede, Callisto, Io, and Europa, are called the 
+    Galilean moons: they show similarities to the terrestrial planets, such as volcanism and 
+    internal heating. Ganymede, the largest satellite in the Solar System, is larger than Mercury; 
+    Callisto is almost as large.`,
   },
   {
     name: "Saturn",
     image: "images/saturn3.png",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Saturn, distinguished by its extensive ring system, has several similarities to 
+    Jupiter, such as its atmospheric composition and magnetosphere. Although Saturn has 
+    60% of Jupiter's volume, it is less than a third as massive, at 95 MEarth. Saturn is 
+    the only planet of the Solar System that is less dense than water. The rings of Saturn 
+    are made up of small ice and rock particles. Saturn has 83 confirmed satellites composed 
+    largely of ice. Two of these, Titan and Enceladus, show signs of geological activity; 
+    they, as well as five other Saturnian moons (Iapetus, Rhea, Dione, Tethys, and Mimas), 
+    are large enough to be round. Titan, the second-largest moon in the Solar System, is 
+    bigger than Mercury and the only satellite in the Solar System to have a substantial atmosphere.`,
   },
   {
     name: "Uranus",
     image: "images/uranus.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Uranus, at 14 MEarth, has the lowest mass of the outer planets. Uniquely among 
+    the planets, it orbits the Sun on its side; its axial tilt is over ninety degrees to 
+    the ecliptic. This gives the planet extreme seasonal variation as each pole points toward 
+    and then away from the Sun. It has a much colder core than the other giant planets and 
+    radiates very little heat into space. As a consequence, it has the coldest planetary 
+    atmosphere in the Solar System. Uranus has 27 known satellites, the largest ones being 
+    Titania, Oberon, Umbriel, Ariel, and Miranda. Like the other giant planets, it possesses 
+    a ring system and magnetosphere.`,
   },
   {
     name: "Neptune",
     image: "images/neptune.jpg",
-    info: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-    laborum eum perferendis atque. Voluptatibus provident dolorem, minima
-    ab exercitationem, molestiae mollitia in quo blanditiis eius, impedit
-    architecto? Officiis laboriosam asperiores earum debitis non,
-    perferendis laborum fugiat pariatur! Rem hic minus nihil iusto,
-    aspernatur nam reiciendis vitae cumque laudantium sed voluptate.`,
+    info: `Neptune, though slightly smaller than Uranus, is more massive (17 MEarth) and 
+    hence more dense. It radiates more internal heat than Uranus, but not as much as Jupiter 
+    or Saturn. Neptune has 14 known satellites. The largest, Triton, is geologically active, 
+    with geysers of liquid nitrogen. Triton is the only large satellite with a retrograde orbit, 
+    which indicates that it did not form with Neptune, but was probably captured from the Kuiper 
+    belt. Neptune is accompanied in its orbit by several minor planets, termed Neptune trojans, 
+    that either lead or trail the planet by about one-sixth of the way around the Sun, positions 
+    known as Lagrange points.`,
   },
 ];
 
